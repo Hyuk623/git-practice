a685853edd7e4133a74e335e92e28146d914d83c
Hyuk623 <chanhyuck623@gmail.com>
2023-05-01 14:45:35 +0900

    docs: Edit README.md
    I wrote README.md to describe what this project is.

---
+++ main.py
@@ -0,0 +1,19 @@
+# Test python env
+def print_hrllo():
+    animals = ['dog', 'cat', hamster', 'tiger'] #in one line
+    foods = [
+        'Spargetti',
+        'Pizza',
+	'Bibimbob'
+    ]# w/o trailing comma	
+    names = [
+	'John', 
+	'Jane', 
+	'Gil-dong',
+	'Dong-eum',
+    ] # w/ trailing comma
+    for f_name in names:
+	ptint(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()
